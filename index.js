ccdb19b69867bfc41403326f4c88b4e3ccabde7c
anuragjatofficial <anurag.1101140@gmail.com>
2023-03-29 03:24:24 +0530

    json server test 3h

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ server.listen(port);
 
 const fs = require('fs')
 const bodyParser = require('body-parser')
-const jsonServer = require('json-server')
+// const jsonServer = require('json-server')
 const jwt = require('jsonwebtoken')
 
 // const server = jsonServer.create()
