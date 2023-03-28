b988d622f37bcb87bde505fbc2a4c549f4220a61
anuragjatofficial <anurag.1101140@gmail.com>
2023-03-29 03:03:30 +0530

    json server for jwt auth

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
-
-// for jwt 
-
 const fs = require('fs')
 const bodyParser = require('body-parser')
 const jsonServer = require('json-server')
