f6dafcbc55a3ee8030ec59d069bb7d7be9ead8e5
pramodjangid <119347747+pramodjangid@users.noreply.github.com>
2023-04-02 21:52:04 +0530

    Update checkout.js

---
+++ checkout.js
@@ -0,0 +1,71 @@
+// let button=document.getElementById("btn");
+
+// button.addEventListener("click",()=>{
+//     location.replace("./payment.html")
+// })
+
+let button1=document.getElementById("btn1");
+
+button1.addEventListener("click",()=>{
+    location.replace("./payment.html")
+})
+
+
+
+// let button1=document.getElementById("btn1");
+
+// button1.addEventListener("click",(e)=>{
+//     e.preventDefault();
+//     if(mobile.value != "" && email.value!= "" && address.value != "" && street.value!= "" && locality.value!= "" && landmark.value!= ""){
+//         myFunction();
+//     }
+//     else {
+//         alert("Fill all the fields")
+//     }
+    
+// })
+
+// button1.addEventListener("click",function(){
+//     if(checkAddress()==true)
+//     window.location.replace("./payment.html")
+// })
+
+
+//     let mobile=document.getElementsByClassName("mobile").value
+// let email=document.getElementsByClassName("email").value
+// let address=document.getElementsByClassName("Address").value
+// let street=document.getElementsByClassName("street").value
+// let locality=document.getElementsByClassName("locality").value
+// let landmark=document.getElementsByClassName("landmark").value
+
+
+
+// function checkAddress(){
+//     let shippingDetails={
+//         "mobile":"mobile.value",
+//         "email": "email.value",
+//         "address":"address.value",
+//         "street":"street.value",
+//         "locality":"locality.value",
+//         "landmark":"landmark",
+//     }
+
+    
+
+//     for(let key in shippingDetails){
+//         if(shippingDetails[key]==undefined){
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+// }
+
+
+// function myFunction() {
+//     setTimeout(() => {
+//         location.replace("./index.html")
+//     }, 5000)
+
+// }
